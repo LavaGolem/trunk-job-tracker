a57717405bb9f8b78f4b5c9fea28283f9261f688
Irma Jakic <irma.jakic@gmail.com>
2023-02-28 21:26:22 +0100

    Updates

--- src/data/types.ts
+++
@@ -1,8 +0,0 @@
-export interface JobEvent {
-  job_event_id: string;
-  job_card_id: string;
-  job_event_type: string;
-  job_event_occurrence: string;
-  job_event_user: string;
-  job_event_metadata: {};
-}
